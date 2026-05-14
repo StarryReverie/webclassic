6ec8666a12090b2b9a2d32b4920d6834d610e182
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-05-14 23:08:37 +0800

    feat(service): allow early-termination if interrupted in service IO

--- components/webclassic-service/src/response.rs
+++ components/webclassic-service/src/response.rs
@@ -2,6 +2,8 @@ use std::io::{Error as IoError, Write};
 
 use snafu::{ResultExt, Snafu};
 
+use crate::Interrupt;
+
 pub trait Response {
     fn serialize(&self) -> Vec<u8>;
 }
@@ -20,10 +22,18 @@ impl<S> ResponseWriter<S>
 where
     S: Write,
 {
-    pub fn write_response<R>(&mut self, response: &R) -> Result<(), WriteResponseError>
+    pub fn write_response<R>(
+        &mut self,
+        response: &R,
+        interrupt: &Interrupt,
+    ) -> Result<(), WriteResponseError>
     where
         R: Response,
     {
+        if interrupt.is_interrupted() {
+            return InterruptedSnafu.fail();
+        }
+
         let data = response.serialize();
         self.sink.write_all(&data).context(WriteSnafu)?;
         self.sink.flush().context(WriteSnafu)?;
@@ -36,4 +46,41 @@ where
 pub enum WriteResponseError {
     #[snafu(display("could not write response data"))]
     Write { source: IoError },
+    #[snafu(display("response writing was interrupted"))]
+    Interrupted,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use crate::InterruptSource;
+
+    use super::*;
+
+    struct BytesResponse(Vec<u8>);
+
+    impl Response for BytesResponse {
+        fn serialize(&self) -> Vec<u8> {
+            self.0.clone()
+        }
+    }
+
+    #[test]
+    fn write_response_success() {
+        let source = InterruptSource::new();
+        let mut writer = ResponseWriter::new(Cursor::new(Vec::new()));
+        writer
+            .write_response(&BytesResponse(b"hello".to_vec()), &source.subscribe())
+            .unwrap();
+    }
+
+    #[test]
+    fn write_response_interrupted() {
+        let source = InterruptSource::new();
+        source.trigger();
+        let mut writer = ResponseWriter::new(Cursor::new(Vec::new()));
+        let result = writer.write_response(&BytesResponse(b"hello".to_vec()), &source.subscribe());
+        assert!(matches!(result, Err(WriteResponseError::Interrupted)));
+    }
 }
