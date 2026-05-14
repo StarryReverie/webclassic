3ae10e99c9b4164f9966beb7a1d8bcfbce8a01ad
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-05-14 22:51:41 +0800

    feat(service): add response and general response writer

--- components/webclassic-service/src/lib.rs
+++ components/webclassic-service/src/lib.rs
@@ -1,5 +1,7 @@
 mod interrupt;
 mod request;
+mod response;
 
 pub use interrupt::{Interrupt, InterruptSource};
 pub use request::{ReadRequestError, Request, RequestReader};
+pub use response::{Response, ResponseWriter, WriteResponseError};
