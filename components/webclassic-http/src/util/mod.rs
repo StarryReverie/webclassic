cca086c712d7b46801aab4c8b4cc166d4eca46ce
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-05-15 12:19:47 +0800

    feat(components/webclassic-http): add StatusCode

--- components/webclassic-http/src/util/mod.rs
+++ components/webclassic-http/src/util/mod.rs
@@ -3,10 +3,12 @@ mod header_name;
 mod method;
 mod percent_decode;
 mod query_map;
+mod status_code;
 mod uri;
 
 pub use header_map::HeaderMap;
 pub use header_name::{HeaderName, ParseHeaderNameError};
 pub use method::{Method, ParseMethodError};
 pub use query_map::{ParseQueryMapError, QueryMap};
+pub use status_code::StatusCode;
 pub use uri::{ParseUriError, Uri};
