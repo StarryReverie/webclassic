e3aa6ce03ef7cb3447f2cf4d40262497121333e6
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-05-15 21:47:02 +0800

    feat(handler-core): add RedirectHandler

---
+++ components/webclassic-handler-core/src/redirect.rs
@@ -0,0 +1,37 @@
+use webclassic_http::request::HttpRequest;
+use webclassic_http::response::HttpResponse;
+use webclassic_http::util::StatusCode;
+use webclassic_service::interrupt::Interrupt;
+use webclassic_web::controller::Controller;
+
+#[derive(Debug, Clone)]
+pub struct RedirectHandler {
+    status: StatusCode,
+    location: String,
+}
+
+impl RedirectHandler {
+    pub fn permanent(location: String) -> Self {
+        Self {
+            status: StatusCode::MOVED_PERMANENTLY,
+            location,
+        }
+    }
+
+    pub fn temporary(location: String) -> Self {
+        Self {
+            status: StatusCode::FOUND,
+            location,
+        }
+    }
+}
+
+impl Controller for RedirectHandler {
+    fn process(&self, _request: HttpRequest, _interrupt: &Interrupt) -> Option<HttpResponse> {
+        Some(
+            HttpResponse::new(self.status)
+                .with_header("location", self.location.clone())
+                .with_body(Vec::new()),
+        )
+    }
+}
