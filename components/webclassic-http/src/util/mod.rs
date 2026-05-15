eaabf3c4ecc5f887a16ef5b9e9ef9b78a7ae2002
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-05-15 11:47:23 +0800

    feat(components/webclassic-http): add HeaderMap

--- components/webclassic-http/src/util/mod.rs
+++ components/webclassic-http/src/util/mod.rs
@@ -1,5 +1,7 @@
+mod header_map;
 mod header_name;
 mod method;
 
+pub use header_map::HeaderMap;
 pub use header_name::{HeaderName, ParseHeaderNameError};
 pub use method::{Method, ParseMethodError};
